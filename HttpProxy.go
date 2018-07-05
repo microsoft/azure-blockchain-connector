c0f7e6e277df674b391e37a5980e71d8d2f65ad6
Xinlei Cao <t-xincao@microsoft.com>
2018-07-05 11:39:10 +0800

    commit

--- HttpProxy.go
+++ HttpProxy.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -17,14 +20,41 @@ type httpProxyParameter struct {
 func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	//req.URL.
 	req.URL.Scheme = "https"
-	//req.URL.Host = "api.github.com"
+	// req.URL.Host = "api.github.com"
+	// req.URL.Host = "127.0.0.1:1235"
+	// req.URL.Host = "104.215.148.235:1234"
 	req.URL.Host = proxyParameter.host
 	req.Host = ""
 	req.RequestURI = ""
 	req.Header.Set("Accept-Encoding", "identity")
-	client := &http.Client{}
-	response, _ := client.Do(req)
+
+	// ca cert setting
+	pool := x509.NewCertPool()
+	caCertPath := "ca.crt"
+	caCrt, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		fmt.Println("ReadFile err:", err)
+		return
+	}
+	pool.AppendCertsFromPEM(caCrt)
+
+	// client setting
+	tp := &http.Transport{
+		TLSClientConfig: &tls.Config{RootCAs: pool},
+		// 	// skip verify
+		// 	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tp}
+
+	// do request and get response
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
 	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
 	io.Copy(rw, response.Body)
 }
 
