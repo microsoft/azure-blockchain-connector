85c9cf6fad301d8bef13bd8d266c09bf3d92b116
Lingfu Che <t-liche@microsoft.com>
2018-07-12 13:44:33 +0800

    change some interfaces

--- HttpProxy.go
+++ HttpProxy.go
@@ -23,7 +23,6 @@ type httpProxyParameter struct {
 
 func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	req.URL.Scheme = "https"
-	// req.URL.Host = "104.215.148.235:1234"
 	req.URL.Host = proxyParameter.host
 	req.Host = ""
 	req.RequestURI = ""
@@ -43,9 +42,9 @@ func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 func initParameter() {
 
 	flag.StringVar(&proxyParameter.port, "port", "1234", "The port you want to listen")
-	flag.StringVar(&proxyParameter.host, "host", "104.215.148.235:2333", "The host you want to send to")
+	flag.StringVar(&proxyParameter.host, "host", "104.215.148.235:3200", "The host you want to send to")
 	flag.StringVar(&username, "username", "root", "The username you want to login with")
-	flag.StringVar(&password, "password", "12345677", "The password you want to login with")
+	flag.StringVar(&password, "password", "123456", "The password you want to login with")
 	flag.Parse()
 	fmt.Print("The aim port is: ")
 	fmt.Println(proxyParameter.port)
@@ -53,16 +52,18 @@ func initParameter() {
 	fmt.Println(proxyParameter.host)
 }
 
-func initClient() {
+func initClient(caCertPath string) {
 	// ca cert setting
 	pool = x509.NewCertPool()
-	caCertPath := "ca.crt"
-	caCrt, err := ioutil.ReadFile(caCertPath)
-	if err != nil {
-		fmt.Println("ReadFile err:", err)
-		return
+	//caCertPath := "ca.crt"
+	if caCertPath != "" {
+		caCrt, err := ioutil.ReadFile(caCertPath)
+		if err != nil {
+			fmt.Println("ReadFile err:", err)
+			return
+		}
+		pool.AppendCertsFromPEM(caCrt)
 	}
-	pool.AppendCertsFromPEM(caCrt)
 
 	// client setting
 	tp = &http.Transport{
@@ -73,13 +74,12 @@ func initClient() {
 }
 
 func main() {
-	/*proxyParameter := httpProxyParameter{
-		//port: "1234",
-		//host: "api.github.com",
-	}*/
-
 	initParameter()
-	initClient()
+
+	//if we don't need to inject any crt, set caCertPath=""
+	caCertPath := "ca.crt"
+	initClient(caCertPath)
+
 	//fmt.Print(proxyParameter)
 	http.HandleFunc("/", ServeHTTP)
 	fmt.Println("Listen on 127.0.0.1:" + proxyParameter.port)
